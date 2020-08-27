bef53daa6ab67afd5f5b2a1b08763fdeca757d9b
Kshitish Deshpande <43273771+ksdfg@users.noreply.github.com>
2020-08-27 22:05:37 +0530

    cleaning up and modularity (#1)
    * add .gitignore
    
    * use proper snake case, cuz python
    
    * add sample_config.ini so we can make changes and commit stuff without committing our config vars
    
    * allow user to select what links he wants to check for
    
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- helpers/wp.py
+++ helpers/wp.py
@@ -1,15 +1,20 @@
 import asyncio
+import configparser
 import os
-import json
+from signal import SIGINT
+
 from webwhatsapi.async_driver import WhatsAPIDriverAsync
 from webwhatsapi.objects.message import Message
-from signal import SIGINT
-from helpers.telegram import Telegram
+
 from helpers.DB import DB
+from helpers.telegram import Telegram
 
 
 class Whatsapp:
     def __init__(self, loop):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        self._links_to_check = list(map(lambda x: x.strip(), config['config']['Links-to-Check'].split(",")))
         self._loop = loop
         self._db = DB()
         self._driver = None
@@ -20,7 +25,7 @@ class Whatsapp:
             os.makedirs(self._config_dir)
 
     async def make(self):
-        self._db.savejson()
+        self._db.save_json()
         await self.sleep(10)
         self._driver = WhatsAPIDriverAsync(loadstyles=True, loop=self._loop, profile=self._config_dir, client="remote",
                                            command_executor=os.environ["SELENIUM"])
@@ -31,41 +36,43 @@ class Whatsapp:
     async def start(self):
         await self.make()
         self._loop.add_signal_handler(SIGINT, self.stop)
-        task1 = self.monitorMessages()
+        task1 = self.monitor_messages()
         await asyncio.wait([task1], loop=self._loop)
 
-    async def monitorMessages(self):
+    async def monitor_messages(self):
+        print(self._links_to_check)
         print("Connecting...")
         await self._driver.connect()
         print("Wait for login...")
         await self._driver.wait_for_login()
         await self._driver.save_firefox_profile(remove_old=True)
-        self._db.addjson()
+        self._db.add_json()
         while True:
             print("Checking for more messages, status", await self._driver.get_status())
-            for cnt in await self.getUnReadMessages():
+            for cnt in await self.get_unread_messages():
                 if self.is_cancelled:
                     break
                 for message in cnt.messages:
                     if isinstance(message, Message):
                         shit = message.get_js_obj()
-                        name = message.sender.name
+                        name = message.sender.push_name
                         if name is None:
                             name = message.sender.get_safe_name()
-                        if shit['chat']['isGroup'] == True:
-                            name = shit['chat']['contact']['formattedName']
-                        if "//meet.google.com/" in message.content:
-                            try:
-                                self._tg.msg_channel(name, shit['sender']['id']['user'], message.content)
-                            except Exception as e:
-                                self._tg.message("New invite link failed to deliver!, Check phone asap | error message = {}".format(e))
+                        chat = shit['chat']['contact']['formattedName']
+
+                        for link in self._links_to_check:
+                            if link.lower() in message.content.lower():
+                                try:
+                                    self._tg.msg_channel(chat, name, message.content)
+                                except Exception as e:
+                                    self._tg.message("New invite link failed to deliver!, Check phone asap | error message = {}".format(e))
 
             await self.sleep(3)
 
     def stop(self, *args, **kwargs):
         self.is_cancelled = True
 
-    async def getUnReadMessages(self):
+    async def get_unread_messages(self):
         cnts = []
         for contact in await self._driver.get_unread():
             print(f"Found Contact: {contact}")
